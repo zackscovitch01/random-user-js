cb7b23398a9964e4ea9179e6c2610bbed17c3473
zackscovitch01 <bagoudoumoubarack@icloud.com>
2024-05-11 09:15:04 -0400

    removeActive function

--- utils/displayUser.js
+++ utils/displayUser.js
@@ -8,21 +8,23 @@ const displayUser = (person) => {
   img.src = person.image;
   value.textContent = person.name;
   title.textContent = `My name is`;
-  btns.forEach((btn) => {
-    btn.classList.remove("active");
-  });
+  removeActive(btns);
   btns[0].classList.add("active");
   btns.forEach((btn) => {
     const label = btn.dataset.label;
     btn.addEventListener("click", () => {
       title.textContent = `My ${label} is`;
       value.textContent = person[label];
-      btns.forEach((btn) => {
-        btn.classList.remove("active");
-      });
+      removeActive(btns);
       btn.classList.add("active");
     });
   });
 };
+
+function removeActive(items) {
+  items.forEach((item) => {
+    item.classList.remove("active");
+  });
+}
 
 export default displayUser;
