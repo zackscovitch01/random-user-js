7e6916fd0f34f4eb711e1513c0da15ce9a292578
zackscovitch01 <bagoudoumoubarack@icloud.com>
2024-05-11 09:06:04 -0400

    displayUser()

--- app.js
+++ app.js
@@ -1,18 +1,13 @@
 import get from "./utils/getElement.js";
 import getUser from "./utils/getUser.js";
+import displayUser from "./utils/displayUser.js";
 
-const img = get(".user-img");
-const title = get(".user-title");
-const value = get(".user-value");
 const btn = get(".btn");
-const btns = [...document.querySelectorAll(".icon")];
 
 const showUser = async () => {
   //   get user from api
   const person = await getUser();
-  console.log(person);
-
-  //
+  displayUser(person);
 };
 
 window.addEventListener("DOMContentLoaded", showUser);
